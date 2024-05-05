3d005dbc665491aa9679baec95f31aa91bf52777
Ray Tran <ray@artran.co.uk>
2024-05-05 19:12:04 +0100

    Get Waybar working

--- home.nix
+++ home.nix
@@ -60,6 +60,53 @@
     '';
   };
 
+  programs.waybar = {
+    enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 30;
+        output = [
+          "eDP-1"
+          "HDMI-A-1"
+        ];
+        modules-left = [ "sway/workspaces" "sway/mode" "wlr/taskbar" ];
+        modules-center = [ "sway/window" "custom/hello-from-waybar" ];
+        modules-right = [ "mpd" "custom/mymodule#with-css-id" "temperature" ];
+
+        "hyprland/workspaces" = {
+          disable-scroll = true;
+          all-outputs = true;
+        };
+        "custom/hello-from-waybar" = {
+          format = "hello {}";
+          max-length = 40;
+          interval = "once";
+          exec = pkgs.writeShellScript "hello-from-waybar" ''
+            echo "from within waybar"
+          '';
+        };
+      };
+    };
+    style = ''
+      * {
+        border: none;
+        border-radius: 10;
+        font-family: Source Code Pro;
+      }
+      window#waybar {
+        background: #16191C;
+        color: #AAB2BF;
+      }
+      #workspaces button {
+        padding: 0 5px;
+      }
+    '';
+  };
+
+
+
   home.sessionVariables = {
     EDITOR = "nvim";
   };
